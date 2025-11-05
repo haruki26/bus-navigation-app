9047ef7baf27de61fe388c6de16c2883b4e0da17
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-11-05 12:05:42 +0900

    Initial commit

---
+++ test/bus-app-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BusAppBackend from '../lib/bus-app-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bus-app-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BusAppBackend.BusAppBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
